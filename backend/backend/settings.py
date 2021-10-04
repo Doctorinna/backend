b54c01e0d7e85809be24a4292a0f480b8c985d3d
danis <aldanis73@yandex.ru>
2021-10-04 18:06:34 +0300

    + postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB', 'doctorinna'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
