ae13136e0c40b9642626b88a5457c974de0e9ebf
danis <aldanis73@yandex.ru>
2021-10-05 23:14:00 +0300

    + populate diseases

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ sleep 10
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py populate
+
 DJANGO_SUPERUSER_PASSWORD="$ADMIN_PASSWORD" python manage.py createsuperuser --username "$ADMIN_USERNAME" --email "$ADMIN_EMAIL" --noinput
 
 python manage.py collectstatic --noinput
