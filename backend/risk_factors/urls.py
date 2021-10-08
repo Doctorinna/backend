057d43c82a78a1c5327327e06d8868228e74d7f2
danis <aldanis73@yandex.ru>
2021-10-09 01:53:20 +0300

    + Result model

--- backend/risk_factors/urls.py
+++ backend/risk_factors/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('diseases/', views.diseases_list, name='diseases'),
     path('categories/', views.categories_list, name='categories'),
     path('questions/', views.questions_list, name='questions'),
-    path('questions/<category>/', views.questions_list, name='questions'),
+    path('questions/<category>', views.questions_list, name='questions'),
     path('response/', views.submit_response, name='response'),
     path('response/<question>', views.change_response, name='response'),
+    path('result/', views.get_result, name='result'),
+    path('result/<disease>', views.get_result, name='result'),
 ]
